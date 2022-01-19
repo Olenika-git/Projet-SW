819c59efa280aafbc4e127b7de82fff7e59505cf
Akinelo <akinelo571@gmail.com>
2022-01-19 16:19:03 +0100

    Mise a jour du Jour Bonjour

--- resource/js/index.js
+++ resource/js/index.js
@@ -17,7 +17,7 @@ function rotationCarousel() {
     rotateFn + '(' + angle + 'deg)';
 }
 
-//Récup des evénements sur les boutons
+// Récup des evénements sur les boutons
 var prevButton = document.querySelector('.precedent');
 prevButton.addEventListener( 'click', function() {
   indexChoisit--;
@@ -30,20 +30,19 @@ nextButton.addEventListener( 'click', function() {
   rotationCarousel();
 });
 
-//Récup des événements sur le slider nbCellule
-var cellsRange = document.querySelector('.nb-Cellule');
-cellsRange.addEventListener( 'change', changeCarousel );
-cellsRange.addEventListener( 'input', changeCarousel );
+// Récup des événements sur le slider nbCellule
+
 
 
 // Changement du nb de cases du Carousel
 function changeCarousel() {
-  nbCellule = cellsRange.value;
+  nbCellule = 9;
   degres = 360 / nbCellule;
   //  tailleCellule est Horizontale ? si oui prend CellLargeur | si non prend CellHauteur
   var tailleCellule = isHorizontal ? cellLargeur : cellHauteur;
   radius = Math.round( ( tailleCellule / 2) / Math.tan( Math.PI / nbCellule ) );
-  for ( var i=0; i < cellule.length; i++ ) {
+
+  for ( var i = 0; i < cellule.length; i++ ) {
     var cell = cellule[i];
     if ( i < nbCellule ) {
       // Cellule Affiché
@@ -58,51 +57,16 @@ function changeCarousel() {
   }
   rotationCarousel();
 }
+// Permet l'initialisation du carousel
+changeCarousel();
 
-//Orientation du Carousel
-var orientationRadio = document.querySelectorAll('input[name="orientation"]');
-( function() {
-  for ( var i = 0; i < orientationRadio.length; i++) {
-    var radio = orientationRadio[i];
-    radio.addEventListener( 'change', onOrientationChange );
-  }
-})();
+// Requêtes API
+$.ajax({
+  url: "https://swapi.dev/api/films/",
+  method: "GET",
+  success: function(response){
+    console.log(response);
+  },
+});
 
-// Au Changement d'orientation du Carousel
-function onOrientationChange() {
-  var checkedRadio = document.querySelector('input[name="orientation"]:checked');
-  isHorizontal = checkedRadio.value == 'horizontal';
-  rotateFn = isHorizontal ? 'rotateY' : 'rotateX';
-  changeCarousel();
-}
-
-// Lancement Initial (Si non utilisé : Suivant et Precedent deviennent unitilisable sans avoir fait une rotation du carousel)
-onOrientationChange();
-
-//Google Scrap
-
-google.load('search', '1');
-google.setOnLoadCallback(OnLoad);
-var search;
-
-    var keyword = 'mountains';
-
-    function OnLoad()
-    {
-        search = new google.search.ImageSearch();
-
-        search.setSearchCompleteCallback(this, searchComplete, null);
-
-        search.execute(keyword);
-    }
-
-    function searchComplete()
-    {
-        if (search.results && search.results.length > 0)
-        {
-            var rnd = Math.floor(Math.random() * search.results.length);
-
-            //Jquery temp $('body').css('background-image', "url('" + search.results[rnd]['url'] + "')");
-            document.body.style.backgroundImage = "url('" + search.results[rnd]['url'] + "')";
-        }
-    }
+// Google Scrap
