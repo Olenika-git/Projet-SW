38ad63d12533141badce425e10dc860ef832aed9
Akinelo <akinelo571@gmail.com>
2022-01-22 22:35:07 +0100

    Push de Perceverance

--- resource/js/index.js
+++ resource/js/index.js
@@ -62,23 +62,40 @@ personnage.addEventListener('click', function () {
     jsdiv = document.querySelector('#infoJs' + i);
     jsdiv.innerHTML = "<div id='js'>" + tableauNomPerso[i] +
       "<p>Taille : " + tableauTaillePerso[i] + "cm" + " </p><p>Poids : " + tableauPoidsPerso[i] + "kg" + "</p><p>Sexe : " + tableauSexePerso[i] + "</p></div>";
-
   }
+  case0.style.backgroundImage = "url('/resource/img/personnage/anakin.webp')";
+  case1.style.backgroundImage = "url('/resource/img/personnage/c3po.png')";
+  case2.style.backgroundImage = "url('/resource/img/personnage/r2d2.jpg')";
+  case3.style.backgroundImage = "url('/resource/img/personnage/darthVader.jpg')";
+  case4.style.backgroundImage = "url('/resource/img/personnage/leia.webp')";
+  case5.style.backgroundImage = "url('/resource/img/personnage/Owenlars.webp')";
+  case6.style.backgroundImage = "url('/resource/img/personnage/beru.webp')";
+  case7.style.backgroundImage = "url('/resource/img/personnage/r5d4.jpg')";
+  case8.style.backgroundImage = "url('/resource/img/personnage/biggs.webp')";
+  jsdiv = document.querySelector('#infoJs');
 });
 
 // Recup Bouton Film
 let film = document.querySelector('#film');
 film.addEventListener('click', function () {
   jsdiv = document.querySelector('#infoJs');
-  console.log("Films");
-  jsdiv.innerHTML = "<div id='js'>" + tableauTitreFilms[0] +
-    "<p>Sortie : " + tableauSortieFilms[0] + " </p><p>Producteur : " + tableauProducteurFilms[0] + "</p><p>Directeur : " + tableauDirecteurFilms[0] + "</p></div>";
   case0.style.backgroundImage = "url('/resource/img/film/film1.jpg')";
   case1.style.backgroundImage = "url('/resource/img/film/film2.jpg')";
   case2.style.backgroundImage = "url('/resource/img/film/film3.jpg')";
   case3.style.backgroundImage = "url('/resource/img/film/film4.jpg')";
   case4.style.backgroundImage = "url('/resource/img/film/film5.jpg')";
   case5.style.backgroundImage = "url('/resource/img/film/film6.jpg')";
+  case6.style.backgroundImage = "url('/resource/img/film/film7.jpg')";
+  case7.style.backgroundImage = "url('/resource/img/film/film8.jpg')";
+  case8.style.backgroundImage = "url('/resource/img/film/film9.jpg')";
+  jsdiv.innerHTML = "<div id='js'>" + tableauTitreFilms[0] +
+    "<p>Sortie : " + tableauSortieFilms[0] + " </p><p>Producteur : " + tableauProducteurFilms[0] + "</p><p>Directeur : " + tableauDirecteurFilms[0] + "</p></div>";
+  for (let i = 1; i < 9; i++) {
+    jsdiv = document.querySelector('#infoJs' + i);
+    jsdiv.innerHTML = "<div id='js'>" + tableauTitreFilms[i] +
+      "<p>Sortie : " + tableauSortieFilms[i] + " </p><p>Producteur : </p><p>" + tableauProducteurFilms[i] + "</p><p>Directeur : </p><p>" + tableauDirecteurFilms[i] + "</p></div>";
+  }
+  jsdiv = document.querySelector('#infoJs');
 });
 
 // Recup Bouton Vaisseau
@@ -113,8 +130,7 @@ $.ajax({
   url: "https://swapi.dev/api/people/",
   method: "GET",
   success: function (responsePerso) {
-    // Ont crée une variable tableauPerso pour stocker nos personnages dans un Tableau
-    // On stocke nos personnages dans le Tableau
+    // On stocke nos données dans les Tableau
     for (let i = 0; i < 9; i++) {
       tableauNomPerso[i] = responsePerso.results[i].name;
       tableauTaillePerso[i] = responsePerso.results[i].height;
@@ -129,16 +145,17 @@ $.ajax({
   url: "https://swapi.dev/api/films/",
   method: "GET",
   success: function (responseFilms) {
-    // Ont crée une variable tableauPerso pour stocker nos personnages dans un Tableau
-    //On stocke nos personnages dans le Tableau
+    // On stocke nos données dans les Tableau
     for (let i = 0; i < 6; i++) {
       tableauTitreFilms[i] = responseFilms.results[i].title;
       tableauSortieFilms[i] = responseFilms.results[i].release_date;
       tableauProducteurFilms[i] = responseFilms.results[i].producer;
       tableauDirecteurFilms[i] = responseFilms.results[i].director;
     }
-    console.log(responseFilms);
-    console.log("Tableau : " + tableauTitreFilms);
+    // Ajout Manuel pour les trois derniers éléments qui ne sont pas générer en films par l'API
+    tableauTitreFilms[6] = "Clone Wars"; tableauSortieFilms[6] = "2008-03-10"; tableauProducteurFilms[6] = 'Kevin Kiner'; tableauDirecteurFilms[6] = "George Lucas";
+    tableauTitreFilms[7] = "Le Mandalorien"; tableauSortieFilms[7] = "2019-12-11"; tableauProducteurFilms[7] = 'Pedro Pascal'; tableauDirecteurFilms[7] = "Jon Favreau";
+    tableauTitreFilms[8] = "The Book of Boba Fett"; tableauSortieFilms[8] = "2021-29-12"; tableauProducteurFilms[8] = 'John Bartnicki'; tableauDirecteurFilms[8] = "Jon Favreau";
   },
 });
 
